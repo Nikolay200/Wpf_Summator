0dd4e276eeec29443a7f7309679bdf8b5852b88b
Nikolay <kolekdoma@mail.ru>
2023-11-09 13:52:01 +0300

    Add project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace WPF_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }        
+    }
+}
